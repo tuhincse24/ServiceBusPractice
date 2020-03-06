e3626a40e4e039045380a0c9c70d09adf275a44e
tuhin <tuhin@test.com>
2020-03-06 19:12:17 +0600

    IQueueClient added

--- ServiceBusPractice/Program.cs
+++ ServiceBusPractice/Program.cs
@@ -8,6 +8,9 @@ namespace ServiceBusPractice
 {
     class Program
     {
+        const string ServiceBusConnectionString = "<your_connection_string>";
+        const string QueueName = "<your_queue_name>";
+        static IQueueClient queueClient;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
