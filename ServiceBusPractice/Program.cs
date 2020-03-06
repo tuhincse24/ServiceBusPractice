8a6bc6044e70c72ca5d28f983720212f9fe14df6
tuhin <tuhin@test.com>
2020-03-06 13:04:52 +0600

    Add project files.

---
+++ ServiceBusPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceBusPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
